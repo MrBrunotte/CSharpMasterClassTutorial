8857e62f1f9abe4139b814a0fcabef7925a1308f
StefanBrunotte <stefan.brunotte@prog-it.se>
2022-01-24 10:38:30 +0100

    39-41

---
+++ Challenge_1_section_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Challenge_1_section_3
+{
+    /* Challenge - Methods
+     * create three variables with names of your friends.
+     * create a Method "GreetFriend" which writes something like: 
+     * "Hi Frank, my friend!" onto the console, once it is called.
+     * Where "Frank" should be replaced with the Name behind the argument 
+     * given to the Method when it's called. So the method will need a parameter 
+     * (decide which DataType will be best). 
+     * 
+     * Greet all your three friends.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend = "Stefan";
+            string friend2 = "Viggo";
+            string friend3 = "Rebecca";
+            Console.WriteLine("Without returning anything - Void");
+            GreetFriendVoid(friend);
+            GreetFriendVoid(friend2);
+            GreetFriendVoid(friend3);
+
+            Console.WriteLine("\nWith Return statement");
+            Console.WriteLine(GreetFriend(friend));
+            Console.WriteLine(GreetFriend(friend2));
+            Console.WriteLine(GreetFriend(friend3));
+           
+            Console.WriteLine("\nWith multiple paramaters");
+            GreetFriendManyThreeNames(friend, friend2, friend3);
+
+            Console.Read();
+        }
+        public static void GreetFriendVoid(string name)
+        {
+            Console.WriteLine($"Hi {name}, my friend!");
+        }
+        public static string GreetFriend(string name)
+        {
+            return $"Hi {name}, my friend!";
+        }
+
+        public static void GreetFriendManyThreeNames(string name1, string name2, string name3)
+        {
+            Console.WriteLine($"Hi {name1}, my friend");
+            Console.WriteLine($"Hi {name2}, my friend");
+            Console.WriteLine($"Hi {name3}, my friend");
+        }
+    }
+}
--- Section_3_FunctionsMethods/Program.cs
+++ Section_3_FunctionsMethods/Program.cs
@@ -6,7 +6,32 @@ namespace Section_3_FunctionsMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int result = Add(15, 32);
+            Console.WriteLine(Add(Add(3, 10), Add(2, 5)));
+            Console.WriteLine(Add(15, 32));
+            Console.WriteLine(Multiply(1, 2.2));
+            Console.WriteLine(Devide(1.0, 2.0));
+            Console.WriteLine(FullName("Stefan", "Brunotte"));
+            Console.Read();
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        private static double Devide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public static double Multiply(double doubleNum1, double doubleNum2)
+        {
+            return doubleNum1 * doubleNum2;
+        }
+
+        public static string FullName(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
         }
     }
+   
 }
